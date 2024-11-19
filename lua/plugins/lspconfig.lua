e0ab52b514927ca3d695c87e4e937fc424bd0692
Rob Hinst <robert.hinst@crowncastle.com>
2024-11-19 14:45:57 -0700

    Adds yang LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,5 +4,18 @@ return {
     opts = {
       autoformat = false,
     },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local configs = require("lspconfig.configs")
+      local util = require("lspconfig.util")
+      configs.yang_lsp = {
+        default_config = {
+          cmd = { "yang-language-server" },
+          filetypes = { "yang" },
+          root_dir = util.find_git_ancestor,
+        },
+      }
+      require("lspconfig").yang_lsp.setup({})
+    end,
   },
 }
---
+++ lua/plugins/yang-highlighting.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "nathanalderson/yang.vim",
+  },
+}
