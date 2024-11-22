d32751c2c0a5447da67db4d1b10667a9bdbe526b
Rob Hinst <robert.hinst@crowncastle.com>
2024-11-22 15:51:06 -0700

    Adds intelephense support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -18,6 +18,34 @@ return {
       require("lspconfig").yang_lsp.setup({ filetypes = { "yang" } })
 
       require("lspconfig").pyright.setup({})
+
+      require("lspconfig").intelephense.setup({})
     end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      handlers = {
+        function(config)
+          require("mason-nvim-dap").default_setup(config)
+        end,
+        php = function(config)
+          config.configurations = {
+            {
+              type = "php",
+              request = "launch",
+              name = "Listen for XDebug",
+              port = 9000,
+              log = true,
+              pathMappings = {
+                ["/opt/app/"] = vim.fn.getcwd(),
+              },
+              hostname = "0.0.0.0",
+            },
+          }
+          require("mason-nvim-dap").default_setup(config)
+        end,
+      },
+    },
   },
 }
