a34f739c37e175b3473e10c9b28d96e5f118dff5
Rob Hinst <robert.hinst@crowncastle.com>
2025-01-16 15:54:41 -0500

    Disables relative line numbers and PHP xdebug configuration

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -15,37 +15,11 @@ return {
           root_dir = util.find_git_ancestor,
         },
       }
-      require("lspconfig").yang_lsp.setup({ filetypes = { "yang" } })
+      require("lspconfig").yang_lsp.setup({})
 
       require("lspconfig").pyright.setup({})
 
       require("lspconfig").intelephense.setup({})
     end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = {
-      handlers = {
-        function(config)
-          require("mason-nvim-dap").default_setup(config)
-        end,
-        php = function(config)
-          config.configurations = {
-            {
-              type = "php",
-              request = "launch",
-              name = "Listen for XDebug",
-              port = 9000,
-              log = true,
-              pathMappings = {
-                ["/opt/app/"] = vim.fn.getcwd(),
-              },
-              hostname = "0.0.0.0",
-            },
-          }
-          require("mason-nvim-dap").default_setup(config)
-        end,
-      },
-    },
   },
 }
