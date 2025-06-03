327321cddf8f23691d4f416620dea154c3bee16c
Rob Hinst <robert.hinst@crowncastle.com>
2025-06-03 11:45:40 -0400

    Updates lazyvim and adds diagnostic virtual_text

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+vim.diagnostic.config({
+  virtual_text = {
+    severity = vim.diagnostic.severity.ERROR, -- or omit to show all severities
+    source = "if_many", -- shows source if multiple diagnostics
+    spacing = 2,
+    prefix = "●", -- or "■", "▎", "x"
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
